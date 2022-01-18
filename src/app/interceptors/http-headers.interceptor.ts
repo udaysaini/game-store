1d252da13fad74d5c859516923d59ddfc97f5d06
Uday Saini <uday10saini@gmail.com>
2022-01-17 23:20:22 -0500

    Security Fix

--- src/app/interceptors/http-headers.interceptor.ts
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -11,12 +11,11 @@ export class HttpHeadersInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
       req = req.clone({
         setHeaders: {
-          'x-rapidapi-key': 'a9dc64443amsh9f073c58500eeadp1822f4jsn64d8fd5cdace',
-          // 'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+          'x-rapidapi-key': 'enter-your-rapid-key',
           'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
         },
         setParams: {
-          key: '22555770ec51443695eb3a9108fb5355',
+          key: 'enter-your-key',
         }
       });
 
